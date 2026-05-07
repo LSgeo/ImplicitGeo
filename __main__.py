c07e916d612516ee91b1d40d4de7d39c74f861cb
Luke <lukesmith.geo@gmail.com>
2026-05-07 13:30:49 +0800

    Debug paths

--- __main__.py
+++ __main__.py
@@ -5,7 +5,7 @@ If we can fit a point data to an Implicit Neural Network, we can operate directl
 
 import os
 import random
-from pathlib import Path
+from pathlib import Path`
 
 # os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True" # Maybe not on WSL
 import colorcet as cc
@@ -32,7 +32,7 @@ device = "cuda"
 fig_out = Path("figures")
 fig_out.mkdir(exist_ok=True, parents=True)
 model_outpath = "model.pt"
-working_dir = Path("../")
+working_dir = Path(os.environ["WORKDIR"])
 
 # Berigora Dataset
 shortname = "gswa"
